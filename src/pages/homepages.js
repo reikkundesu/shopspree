c910b404005ac454a422c98d9cde8e69be0f8d28
reikkundesu <manalojosephkitrei@gmail.com>
2024-08-08 12:22:31 +0800

    chore: initial setup

---
+++ src/pages/homepages.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="flex flex-col items-center bg-gray-100 min-h-screen pt-32">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to ShopSpree</h1>
+      <p className="text-lg text-gray-600">Your one-stop shop for stress-free shopping spree!</p>
+    </div>
+  );
+}
+
+export default Home;
