9c8c59d78021a925630664d0607ddda20139c071
reikkundesu <manalojosephkitrei@gmail.com>
2024-08-09 02:06:03 +0800

    refactor: adjusted skeleton size

--- src/components/skeleton.js
+++ src/components/skeleton.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SkeletonCard = () => {
   return (
     <div className="p-4 border rounded-lg bg-white shadow-md animate-pulse">
-      <div className="w-72 h-96 bg-gray-200 rounded-lg mb-4"></div>
+      <div className="w-52 h-96 bg-gray-200 rounded-lg mb-4"></div>
       <div className="h-4 bg-gray-200 rounded mb-2"></div>
       <div className="h-4 bg-gray-200 rounded mb-2"></div>
       <div className="h-4 bg-gray-200 rounded"></div>
